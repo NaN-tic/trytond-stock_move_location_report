2e2846957118ee43d3bb72d9b21d2708a6ea8830
Raimon Esteve <raimonesteve@gmail.com>
2021-10-27 15:25:20 +0200

    Rename stock_traceability_report to stock_move_location_report #041363

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,6 @@
-# This file is part stock_traceability_report module for Tryton.
+# This file is part stock_move_location_report module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-from .test_stock_traceability_report import suite
+from .test_stock_move_location_report import suite
 
 __all__ = ['suite']
